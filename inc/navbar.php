09b414d5d76f4bef21ebcd53f38775b489076c4d
djphil <digitalconcepts@free.fr>
2017-04-24 09:34:57 +0200

    Add dropdow login form
    Add dropdow login form

--- inc/navbar.php
+++ inc/navbar.php
@@ -21,6 +21,44 @@
                         <a href="./?help"><i class="glyphicon glyphicon-education"></i> Help</a>
                     </li>
                 </ul>
+                
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" href="#" data-toggle="dropdown">
+                            <i class="glyphicon glyphicon-user"></i>
+                            <span class="caret"></span>
+                        </a>
+
+                        <div class="dropdown-menu dropdown-lr" style="padding: 15px; padding-bottom: 15px;">
+
+                        <?php if (!isset($_SESSION['valid'])): ?>
+                            <form class="form-horizontal" action="?login" method="post" accept-charset="UTF-8">
+                            <div class="input-group login">
+                                <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                                <input type="text" class="form-control " name="username" placeholder="Username">
+                            </div>
+                            <div class="input-group login">
+                                <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                                <input type="password" class="form-control " name="password" placeholder="Password">
+                            </div>
+                                <button class="btn btn-default btn-block" type="submit" name="login">
+                                    <i class="glyphicon glyphicon-log-in"></i> Login
+                                </button>
+                            </form>
+                        <?php else: ?>
+                            <form class="form-horizontal" action="?logout" method="post" accept-charset="UTF-8">
+                            <div class="input-group login">
+                                <p>Welcome <i class="glyphicon glyphicon-user"></i> <?php echo $_SESSION['username']; ?></p>
+                            </div>
+                                <button class="btn btn-default btn-block" type="submit" name="logout">
+                                    <i class="glyphicon glyphicon-log-out"></i> Logout
+                                </button>
+                            </form>
+                        <?php endif; ?>
+                        </div>
+                    </li>
+                </ul>
+
                 <form class="navbar-form navbar-right" role="search" action="?home" enctype="multipart/form-data" method="POST">
                 <div class="input-group">
                     <input type="text" class="form-control" name="q" id="Search" placeholder="Search" >
